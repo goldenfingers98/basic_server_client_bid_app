1ff9d14021380578a00fa60d8ca1f46c9a0d53e1
goldenfingers98 <tchtourou21@gmail.com>
2021-05-11 12:32:52 +0100

    first commit

---
+++ client/Client.py
@@ -0,0 +1,76 @@
+import socket, sys, os
+from threading import Thread
+from dotenv import dotenv_values
+
+
+# Changing current directory
+os.chdir("\\".join(os.path.realpath(__file__).split('\\')[:-1]))
+
+# initializing host and port
+CONFIG = dotenv_values('./.env')
+HOST =  CONFIG["HOST"]
+PORT = int(CONFIG["PORT"])
+
+class ThreadReception(Thread):
+    """
+        Thread object that receives messages
+    """
+    def __init__(self,conn,emissionThread):
+        Thread.__init__(self)
+        self.connexion = conn
+        self.th_E = emissionThread
+
+    def run(self):
+        while 1:
+            received_message = ""
+            try:
+                received_message = self.connexion.recv(1024)\
+                                        .decode("Utf8")
+            except:
+                break
+            print(f"{received_message}")
+            if received_message.upper() == "END":
+                break
+        self.th_E.stop()
+        print("\n>>>> Client is stoped. Interrupted connexion")
+        self.connexion.close()
+
+class ThreadEmission(Thread):
+    """
+        Thread object that sends messages
+    """
+    def __init__(self,conn):
+        Thread.__init__(self)
+        self.connexion = conn
+        self.alive = True
+
+    def run(self):
+        while self.alive:
+            try:
+                messageToSend = input("Sys>>>> Message to send: ")
+                self.connexion.send(messageToSend.encode("Utf8"))
+            except:
+                print("Sys>>>> Server is down!")
+                break
+
+    def stop(self):
+        self.alive = False
+
+######################################################################
+##########################__Main program__############################
+######################################################################
+
+if __name__ == "__main__":
+    connexion = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        connexion.connect((HOST,PORT))
+    except socket.error:
+        print(">>>> Connexion failed !")
+        sys.exit()
+    else:
+        print(">>>> Connected to the server !")
+    
+    th_E = ThreadEmission(connexion)
+    th_R = ThreadReception(connexion,th_E)
+    th_R.start()
+    th_E.start()
---
+++ server/Server.py
@@ -0,0 +1,85 @@
+from logging import error
+import socket, sys, os
+from threading import Thread
+from dotenv import dotenv_values
+from time import sleep
+
+
+# Changing current directory
+os.chdir("\\".join(os.path.realpath(__file__).split('\\')[:-1]))
+
+CONFIG = dotenv_values('.env')
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = int(CONFIG["PORT"])
+POOL_SIZE = int(CONFIG["POOL_SIZE"])
+client_pool = {} #Client set
+
+class ThreadingClient(Thread):
+    """
+        Thread object for each client
+    """
+    def __init__(self,conn,address):
+        Thread.__init__(self)
+        self.connexion = conn
+        self.isConnected = False
+        self.address = address
+
+    def run(self):
+        # Client status : connected
+        self.isConnected = True
+        #Listening requests from client
+        name = self.getName() #Each client has a specific name
+        while 1:
+            try:
+                msgClient = self.connexion.recv(1024)\
+                                .decode("Utf8")
+            except:
+                break
+            if not msgClient or msgClient.upper() == "END":
+                break
+            message = f"{name} >>>> {msgClient}"
+            print(message)
+            #Broadcast the message to the others
+            for key in client_pool:
+                if key != name:
+                    client_pool[key].send(message.encode('Utf8'))
+            
+        #Closing the connexion
+        try:
+            self.connexion.close()
+            self.isConnected = False
+        except:pass
+        #The client leaves the pool
+        del client_pool[name]
+        print(f">>>> Client {name} disconnected !")
+
+    
+    def __str__(self):
+        if self.isConnected:
+            return f"Client {self.getName()} is connected at port {PORT} with address {self.address}."
+
+######################################################################
+##########################__Main program__############################
+######################################################################
+if __name__ == "__main__":
+    # Server initialisation
+    _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        _socket.bind((HOST,PORT))
+    except socket.error:
+        print(">>>> Socket connexion to the port has failed !")
+        sys.exit()
+    print(">>>> Server is ready to listen...")
+    _socket.listen(POOL_SIZE)
+
+    #Waiting for clients
+    while 1:
+        sleep(0.005)
+        (connexion, address) = _socket.accept()
+        # New client has come
+        client = ThreadingClient(connexion,address)
+        # Reserving a place in the pool
+        client_pool[client.getName()] = connexion
+        client.start()
+        print(f">>>> {client}")
+        connexion.send("SERVER >>>>: You are connected. Ready to listen...".encode('Utf8'))
